e532815aef10ae7b014daa679b401120b7195e81
mateo pèrez <yta123@gmail.com>
2020-02-01 17:53:37 -0500

    inital commit

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Eventually', // <title>
+  manifestName: 'Viajandola',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Viajandola ',
+  subHeading:
+    "La próxima red social para darle vida a tus viajes",
+  socialLinks: [
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: '#',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: '#',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: '#',
+    },
+  ],
+};
---
+++ src/components/EmailForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export class EmailForm extends Component {
+  constructor() {
+    super();
+    this.state = { message: '' };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({ message: 'Gracias' });
+    setTimeout(() => {
+      this.setState({ message: '' });
+    }, 3000);
+  }
+
+  render() {
+    const { message } = this.state;
+    return (
+      <form id="signup-form" onSubmit={this.onSubmit} method="post" action="#">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Tu correo email"
+        />
+        <input type="submit" value="Suscríbete" />
+        <span className={`${message ? 'visible success' : ''} message`}>
+          {message}
+        </span>
+      </form>
+    );
+  }
+}
+
+export default EmailForm;
